8d4115c0b2e0e274b4e3422580f57435934f68b6
Ega-telkom <541241041@student.smktelkom-pwt.sch.id>
2026-05-22 13:39:50 +0700

    fix: add import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Models\Book;
+use Illuminate\Support\Facades\URL;
 use App\Observers\BookObserver;
 
 class AppServiceProvider extends ServiceProvider
